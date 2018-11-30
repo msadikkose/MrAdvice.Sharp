0e8d6f0875738c3a4c28760ac8c512bcc169114a
Muhammed Sadık Köse <msadikkose@gmail.com>
2018-11-30 10:13:52 +0300

    Project added

---
+++ Aspects/OnExceptionAspect.cs
@@ -0,0 +1,27 @@
+﻿using ArxOne.MrAdvice.Advice;
+using MrAdvice.Sharp.Model;
+using System;
+using System.Threading.Tasks;
+
+
+namespace  MrAdvice.Sharp.Aspects
+{
+    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Class)]
+    public abstract class OnExceptionAspect : Attribute, IMethodAsyncAdvice
+    {
+        public async Task Advise(MethodAsyncAdviceContext context)
+        {
+            try
+            {
+                await context.ProceedAsync();
+            }
+            catch (Exception ex)
+            {
+                MethodExecutionArgs args = new MethodExecutionArgs(context, ex);
+                OnException(args);
+            }
+        }
+
+        public abstract void OnException(MethodExecutionArgs args);
+    }
+}
---
+++ Model/MethodExecutionArgs.cs
@@ -0,0 +1,54 @@
+﻿using ArxOne.MrAdvice.Advice;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace MrAdvice.Sharp.Model
+{
+    public class MethodExecutionArgs
+    {
+        public Exception Exception { get; private set; }
+
+        public IList<object> Arguments { get; private set; }
+        public bool HasReturnValue { get; private set; }
+        public object ReturnValue { get; private set; }
+        public MethodBase TargetMethod { get; private set; }
+        public string TargetName { get; private set; }
+        public bool IsTargetMethodAsync { get; private set; }
+        public MethodAsyncAdviceContext Context { get; private set; }
+
+
+
+        public MethodExecutionArgs(MethodAsyncAdviceContext context, Exception exception)
+        {
+            Context = context;
+            Exception = exception;
+            Arguments = context.Arguments;
+            HasReturnValue = context.HasReturnValue;
+            TargetMethod = context.TargetMethod;
+            TargetName = context.TargetName;
+            IsTargetMethodAsync = context.IsTargetMethodAsync;
+            ReturnValue = context.ReturnValue;
+        }
+
+        public void SetReturnValue<T>(T returnValue)
+        {
+            if (IsTargetMethodAsync)
+            {
+                TaskCompletionSource<T> taskCompletionSource = GetTaskCompletionSource(returnValue);
+                Task task  = taskCompletionSource.Task;
+                Context.ReturnValue = task;
+            }
+            else
+                Context.ReturnValue = returnValue;
+        }
+        private TaskCompletionSource<T> GetTaskCompletionSource<T>(T result)
+        {
+            var tcs = new TaskCompletionSource<T>();
+            tcs.SetResult(result);
+            return tcs;
+        }
+
+    }
+}
